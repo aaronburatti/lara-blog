5acfadf6a4eb32699340ae6db2983c82368a5e70
aaronburatti <aaronburatti@gmail.com>
2022-06-16 08:46:46 -0500

    Initial commit for laravel learning project

---
+++ resources/views/post.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+<body>
+<article>
+        <h1><a href="/post">Second Post<a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Iusto itaque sunt enim. Neque quo aspernatur, fugit sequi tenetur 
+            nostrum numquam voluptates? Consequuntur voluptate enim, ab iure consequatur 
+            aut est nemo.
+        </p>
+        </article>
+        <article>
+        <h1><a href="/post">Third Post<a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Iusto itaque sunt enim. Neque quo aspernatur, fugit sequi tenetur 
+            nostrum numquam voluptates? Consequuntur voluptate enim, ab iure consequatur 
+            aut est nemo.
+        </p>
+        </article>
+    <a href="/">Go Back</a>
+</body>
+</html>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+    <title>
+        My Blog
+    </title>
+    <link rel="stylesheet" href="/app.css">
+    <body>
+        <article>
+        <h1><a href="/post">First Post<a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Iusto itaque sunt enim. Neque quo aspernatur, fugit sequi tenetur 
+            nostrum numquam voluptates? Consequuntur voluptate enim, ab iure consequatur 
+            aut est nemo.
+        </p>
+        </article>
+        
+    </body>
